2de972760ae526d693df79844ac1f64ea2cbf60f
hansj4873 <hansengjuu@naver.com>
2026-06-12 07:02:22 +0900

    피보나치 수

---
+++ lessons12945.cs
@@ -0,0 +1,16 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/12945
+//피보나치 수
+public class Solution
+{
+    public int solution(int n)
+    {
+        int[] arr = new int[100001];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            arr[i] = (arr[i - 1] + arr[i - 2]) % 1234567;
+        }
+        return arr[n];
+    }
+}
